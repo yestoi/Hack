166f22ff64cc3a0a6045c236724cd9706defd5e1
yestoi <yestoi1@gmail.com>
2009-09-07 15:21:22 -0400

    clean up.

--- game.py
+++ game.py
@@ -4,16 +4,21 @@ import readline #for bash-like history
 class Game:
     
     def __init__(self):
+        # Temporary solution to loading dicts. Switch to shelve when finished
         import yaml
         file = open('hosts.yml')
         self.hosts = yaml.safe_load(file)
         file.close()
 
-        self.curr_host = "localhost"
+        self.host = "localhost"
+        self.user = ""
+        self.printwd = "/"
 
     def ls(self):
-        print self.hosts[self.curr_host]['files'].keys()
-        
+        print "\n".join(self.hosts[self.host]['files'].keys())
+
+    def pwd(self):
+        print self.printwd 
 
 class CmdInterface(cmd.Cmd):
     """Main commandline interface"""
@@ -26,13 +31,18 @@ class CmdInterface(cmd.Cmd):
        # COMMANDS
     def do_ls(self, line):
         input = line.split()
+
         if len(input) == 1:
             print "not implemented"
         else:
             self.Game.ls()
 
+    def do_pwd(self, line):
+        self.Game.pwd()
+
     def do_exit(self, line):
         return 1 
 
     def do_EOF(self, line):
+        print "\n"
         return 0 #ignore EOFs
